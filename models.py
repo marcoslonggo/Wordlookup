82322f5aa4710499884e819a55225335b266c2c8
Marcos Longo <marcoslongo@gmail.com>
2020-09-06 01:44:41 +0200

    Added other functions(Words, Brands, UoM)

--- models.py
+++ models.py
@@ -42,8 +42,41 @@ class Superlatives(db.Model):
     updated_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     updated_when = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)  
+class Words(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ptpt = db.Column(db.String(120), nullable=False, unique=True)
+    ptbr = db.Column(db.String(120), nullable=False, unique=True)
+    abbreviation = db.Column(db.String(120), nullable=True)
+    ss1 = db.Column(db.String(120), nullable=True)
+    ss2 = db.Column(db.String(120), nullable=True)
+    ss3 = db.Column(db.String(120), nullable=True)
+    comment = db.Column(db.String(120), nullable=True)
+    updated_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    updated_when = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class Brands(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    brandname = db.Column(db.String(120), nullable=False, unique=True)
+    abbreviation = db.Column(db.String(120), nullable=True)
+    ss1 = db.Column(db.String(120), nullable=True)
+    ss2 = db.Column(db.String(120), nullable=True)
+    ss3 = db.Column(db.String(120), nullable=True)
+    comment = db.Column(db.String(120), nullable=True)
+    updated_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    updated_when = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class Uom(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    unit = db.Column(db.String(120), nullable=False, unique=True)
+    symbol = db.Column(db.String(120), nullable=True)
+    description = db.Column(db.String(120), nullable=True)
+    ss1 = db.Column(db.String(120), nullable=True)
+    ss2 = db.Column(db.String(120), nullable=True)
+    ss3 = db.Column(db.String(120), nullable=True)
+    comment = db.Column(db.String(120), nullable=True)
+    updated_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    updated_when = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
 
 @login.user_loader
 def load_user(id):
--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app
-from models import User, Superlatives, Grocery
+from models import User, Superlatives, Words, Brands, Grocery, Uom
 from forms import LoginForm
 from flask_login import logout_user, login_required, current_user, login_user
 from werkzeug.urls import url_parse
@@ -8,6 +8,7 @@ from forms import RegistrationForm
 from app import db
 import sys
 import re
+from sqlalchemy import and_, or_, not_
 import sqlalchemy.exc as sqlalchemy_exc
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -43,15 +44,42 @@ def register():
         flash('Registration complete')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+@app.route('/admin', methods=['GET', 'POST'])
+@login_required
+def admin():
+    if current_user.admin != 1: 
+        return "You are not admin"
 
+    else:
+        users = User.query.order_by(User.username).all()
+        return render_template('admin.html', users=users)
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        new_stuff = Grocery(name=name)
+
+        try:
+            db.session.add(new_stuff)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was a problem adding new stuff."
+
+    else:
+        groceries = Grocery.query.order_by(Grocery.created_at).all()
+        return render_template('index.html', groceries=groceries)
+
+######################SUPERLATIVES################################
 @app.route('/update/superlatives/<int:id>', methods=['GET', 'POST'])
 @login_required
 def update(id):
     superlatives = Superlatives.query.get_or_404(id)
     if request.method == 'POST':
-            
-            
+        if request.form['word'] == '':
+            return "Word cannot be empty"
+        else:
             superlatives.word = request.form['word']
             superlatives.example = request.form['example']
             superlatives.ss1 = request.form['ss1']
@@ -61,11 +89,11 @@ def update(id):
             superlatives.updated_by = request.form['updated_by']
             
 
-            try:
-               db.session.commit()
-               return redirect('/superlatives')
-            except sqlalchemy_exc.SQLAlchemyError: 
-                raise
+        try:
+            db.session.commit()
+            return redirect('/superlatives')
+        except sqlalchemy_exc.SQLAlchemyError: 
+            raise
     else:
         title = "Update Data"
         return render_template('updatesuperlatives.html', title=title, superlatives=superlatives) 
@@ -87,6 +115,9 @@ def delete(id):
 @login_required
 def superlatives():
     if request.method == 'POST':
+        if request.form['word'] == '':
+            return "Word cannot be empty"
+        else:
             word = request.form['word']
             example = request.form['example']
             ss1 = request.form['ss1']
@@ -106,15 +137,7 @@ def superlatives():
         superlatives = Superlatives.query.order_by(Superlatives.updated_when).all()
         return render_template('superlatives.html', superlatives=superlatives)
 
-@app.route('/admin', methods=['GET', 'POST'])
-@login_required
-def admin():
-    if current_user.admin != 1: 
-        return "You are not admin"
 
-    else:
-        users = User.query.order_by(User.username).all()
-        return render_template('admin.html', users=users)
 
 
 @app.route('/superlatives/search' , methods=['POST'])
@@ -122,7 +145,6 @@ def superlativessearch():
     if request.method == 'POST':
         try:                           
             searchword = request.form['search']
-            #superlatives = Superlatives.query.filter_by(word = searchword).all()
             searchword2  = "%{}%".format(searchword)            
             superlatives = Superlatives.query.filter(Superlatives.word.like(searchword2)).all()
             return render_template('superlatives.html', superlatives=superlatives, searchword=searchword)
@@ -133,24 +155,266 @@ def superlativessearch():
         return render_template('superlatives.html', superlatives=superlatives)
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+
+
+
+########Words###########
+@app.route('/update/words/<int:id>', methods=['GET', 'POST'])
+@login_required
+def wordsupdate(id):
+    words = Words.query.get_or_404(id)
     if request.method == 'POST':
-        name = request.form['name']
-        new_stuff = Grocery(name=name)
-
+        if request.form['ptpt'] == '' and request.form['ptbr'] == '':
+            return "Words cannot be empty, either fill word in PT-PT or PT-BR"
+        else:
+            words.ptpt = request.form['ptpt']
+            words.ptbr = request.form['ptbr']
+            words.abbreviation = request.form['abbreviation']
+            words.ss1 = request.form['ss1']
+            words.ss2 = request.form['ss2']
+            words.ss3 = request.form['ss3']
+            words.comment = request.form['comment']
+            words.updated_by = request.form['updated_by']
         try:
-            db.session.add(new_stuff)
             db.session.commit()
-            return redirect('/')
-        except:
-            return "There was a problem adding new stuff."
-
+            return redirect('/words')
+        except sqlalchemy_exc.SQLAlchemyError: 
+            raise
     else:
-        groceries = Grocery.query.order_by(Grocery.created_at).all()
-        return render_template('index.html', groceries=groceries)
+        title = "Update Data"
+        return render_template('updatewords.html', title=title, words=words) 
 
+@app.route('/delete/words/<int:id>')
 #@login_required
-#def index():
-#    return render_template("index.html", title='Home Page')
+def wordsdelete(id):
+        delete = Words.query.get_or_404(id)
+        try:
+            db.session.delete(delete)
+            db.session.commit()
+            return redirect('/words')
+        except:
+            return "There was a problem deleting data"
 
+
+
+@app.route('/words', methods=['GET', 'POST'])
+@login_required
+def words():
+    if request.method == 'POST':
+        if request.form['ptpt'] == '' and request.form['ptbr'] == '':
+            return "Words cannot be empty, either fill word in PT-PT or PT-BR"
+        else:
+            ptpt = request.form['ptpt']
+            ptbr = request.form['ptbr']
+            abbreviation = request.form['abbreviation']
+            ss1 = request.form['ss1']
+            ss2 = request.form['ss2']
+            ss3 = request.form['ss3']
+            comment = request.form['comment']
+
+            words = Words(ptpt=ptpt, ptbr=ptbr, abbreviation=abbreviation, ss1=ss1, ss2=ss2,ss3=ss3, comment=comment, updated_by=current_user.username)
+
+            try:
+                db.session.add(words)
+                db.session.commit()
+                return redirect('/words')
+            except:
+                return "There was a problem adding new words."
+    else:
+        
+        words = Words.query.order_by(Words.updated_when).all()
+        return render_template('words.html', words=words)
+
+
+
+
+@app.route('/words/search' , methods=['POST'])
+def wordssearch():
+    if request.method == 'POST':
+        try:
+            searchword = request.form['search']
+            searchword2  = "%{}%".format(searchword)            
+            words = Words.query.filter(or_((Words.ptbr.like(searchword2)),(Words.ptpt.like(searchword2)),(Words.abbreviation.like(searchword2)))).all()
+            return render_template('words.html', words=words, searchword=searchword)
+        except:
+            return "There was an error searching"
+    else:
+        words = Words.query.order_by(Words.updated_when).all()
+        return render_template('words.html', words=words)
+
+
+
+
+        ########Brands###########
+@app.route('/update/brands/<int:id>', methods=['GET', 'POST'])
+@login_required
+def brandsupdate(id):
+    brands = Brands.query.get_or_404(id)
+    if request.method == 'POST':
+        if request.form['brandname'] == '':
+            return "brands cannot be empty"
+        else:
+            brands.brandname = request.form['brandname']
+            brands.abbreviation = request.form['abbreviation']
+            brands.ss1 = request.form['ss1']
+            brands.ss2 = request.form['ss2']
+            brands.ss3 = request.form['ss3']
+            brands.comment = request.form['comment']
+            brands.updated_by = request.form['updated_by']
+        try:
+            db.session.commit()
+            return redirect('/brands')
+        except sqlalchemy_exc.SQLAlchemyError: 
+            raise
+    else:
+        title = "Update Data"
+        return render_template('updatebrands.html', title=title, brands=brands) 
+
+@app.route('/delete/brands/<int:id>')
+#@login_required
+def brandsdelete(id):
+        delete = Brands.query.get_or_404(id)
+        try:
+            db.session.delete(delete)
+            db.session.commit()
+            return redirect('/brands')
+        except:
+            return "There was a problem deleting data"
+
+
+
+@app.route('/brands', methods=['GET', 'POST'])
+@login_required
+def brands():
+    if request.method == 'POST':
+        if request.form['brandname'] == '':
+            return "brands cannot be empty"
+        else:
+            brandname = request.form['brandname']
+            abbreviation = request.form['abbreviation']
+            ss1 = request.form['ss1']
+            ss2 = request.form['ss2']
+            ss3 = request.form['ss3']
+            comment = request.form['comment']
+
+            brands = Brands(brandname=brandname, abbreviation=abbreviation, ss1=ss1, ss2=ss2,ss3=ss3, comment=comment, updated_by=current_user.username)
+
+            try:
+                db.session.add(brands)
+                db.session.commit()
+                return redirect('/brands')
+            except:
+                return "There was a problem adding new brands."
+    else:
+        
+        brands = Brands.query.order_by(Brands.updated_when).all()
+        return render_template('brands.html', brands=brands)
+
+
+
+
+@app.route('/brands/search' , methods=['POST'])
+def brandssearch():
+    if request.method == 'POST':
+        try:                           
+            searchword = request.form['search']
+            searchword2  = "%{}%".format(searchword)            
+            brands = Brands.query.filter(or_((Brands.brandname.like(searchword2)),(Brands.abbreviation.like(searchword2)))).all()
+            print('FUCLK THIS')
+            print(brands)
+            print(searchword)
+            return render_template('brands.html', brands=brands, searchword=searchword)
+        except:
+            return "There was an error searching"
+    else:
+        brands = Brands.query.order_by(Brands.updated_when).all()
+        return render_template('brands.html', brands=brands)
+
+
+
+
+
+        ########UoM###########
+@app.route('/update/uom/<int:id>', methods=['GET', 'POST'])
+@login_required
+def uomupdate(id):
+    uom = Uom.query.get_or_404(id)
+    if request.method == 'POST':
+        if request.form['unit'] == '':
+            return "uom cannot be empty"
+        else:
+            uom.unit = request.form['unit']
+            uom.symbol = request.form['symbol']
+            uom.description = request.form['description']
+            uom.ss1 = request.form['ss1']
+            uom.ss2 = request.form['ss2']
+            uom.ss3 = request.form['ss3']
+            uom.comment = request.form['comment']
+            uom.updated_by = request.form['updated_by']
+        try:
+            db.session.commit()
+            return redirect('/uom')
+        except sqlalchemy_exc.SQLAlchemyError: 
+            raise
+    else:
+        title = "Update Data"
+        return render_template('updateuom.html', title=title, uom=uom) 
+
+@app.route('/delete/uom/<int:id>')
+#@login_required
+def uomdelete(id):
+        delete = Uom.query.get_or_404(id)
+        try:
+            db.session.delete(delete)
+            db.session.commit()
+            return redirect('/uom')
+        except:
+            return "There was a problem deleting data"
+
+
+
+@app.route('/uom', methods=['GET', 'POST'])
+@login_required
+def uom():
+    if request.method == 'POST':
+        if request.form['unit'] == '':
+            return "uom cannot be empty"
+        else:
+            unit = request.form['unit']
+            symbol = request.form['symbol']
+            description = request.form['description']
+            ss1 = request.form['ss1']
+            ss2 = request.form['ss2']
+            ss3 = request.form['ss3']
+            comment = request.form['comment']
+            
+
+            uom = Uom(unit=unit, symbol=symbol, description=description, ss1=ss1, ss2=ss2,ss3=ss3, comment=comment, updated_by=current_user.username)
+
+            try:
+                db.session.add(uom)
+                db.session.commit()
+                return redirect('/uom')
+            except:
+                return "There was a problem adding new uom."
+    else:
+        
+        uom = Uom.query.order_by(Uom.updated_when).all()
+        return render_template('uom.html', uom=uom)
+
+
+
+
+@app.route('/uom/search' , methods=['POST'])
+def uomsearch():
+    if request.method == 'POST':
+        try:                           
+            searchword = request.form['search']
+            searchword2  = "%{}%".format(searchword)            
+            uom = Uom.query.filter(or_((Uom.unit.like(searchword2)),(Uom.symbol.like(searchword2)),(Uom.description.like(searchword2)))).all()
+            return render_template('uom.html', uom=uom, searchword=searchword)
+        except:
+            return "There was an error searching"
+    else:
+        uom = uom.query.order_by(Uom.updated_when).all()
+        return render_template('uom.html', uom=uom)
