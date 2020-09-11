a0f7492cf9cc50f2125a32ba40baf2c786d2c2c8
Marcos Longo <marcoslongo@gmail.com>
2020-09-11 16:30:52 +0200

    d

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy 
 from flask_login import LoginManager
 from flask_migrate import Migrate
+import psycopg2
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Yabadabadoo'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///wordlookup.db'  
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://ptwordlookup:Pwb9NMabTGX6sHk@ptwordlookup.longo.com.br/ptwordlookup'  
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kmzhrgjwynsquf:8ebd2b217894767fa7b778c43f3ceb539af561e85ef5c53aa6144d8ae0070cf5@ec2-52-21-247-176.compute-1.amazonaws.com:5432/d3gh9b2i23ld11'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app) 
