b36ebb6652cbe5fd378e21e32259b2dcf29f0b7b
Marcos Longo <marcoslongo@gmail.com>
2020-09-11 16:59:32 +0200

    adding db URL

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy 
 from flask_login import LoginManager
 from flask_migrate import Migrate
+import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Yabadabadoo'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kmzhrgjwynsquf:8ebd2b217894767fa7b778c43f3ceb539af561e85ef5c53aa6144d8ae0070cf5@ec2-52-21-247-176.compute-1.amazonaws.com:5432/d3gh9b2i23ld11'
+app.config['SECRET_KEY'] = os.urandom(24)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app) 
 migrate = Migrate(app, db)
