72396d63dc0503b4527b2abbcf0877d468231cc8
Marcos Longo <marcoslongo@gmail.com>
2020-09-11 16:13:22 +0200

    trying to fix postgre

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy 
 from flask_login import LoginManager
 from flask_migrate import Migrate
+import os
+import psycopg2
 
 
 app = Flask(__name__)
